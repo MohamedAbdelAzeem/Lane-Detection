20992d4429bfc6920f1b13c88d1d9936bf309596
MohamedTarek251999 <52556495+Muhammed-El-Sayed@users.noreply.github.com>
2022-04-20 01:36:06 +0200

    Create LaneLines.py

---
+++ LaneLines.py
@@ -0,0 +1,155 @@
+import cv2
+import numpy as np
+import matplotlib.image as mpimg
+
+def hist(img):
+    bottom_half = img[img.shape[0]//2:,:]
+    return np.sum(bottom_half, axis=0)
+
+class LaneLines:
+    """ Class containing information about detected lane lines.
+
+    Attributes:
+        left_fit (np.array): Coefficients of a polynomial that fit left lane line
+        right_fit (np.array): Coefficients of a polynomial that fit right lane line
+        parameters (dict): Dictionary containing all parameters needed for the pipeline
+        debug (boolean): Flag for debug/normal mode
+    """
+    def __init__(self):
+        """Init Lanelines.
+
+        Parameters:
+            left_fit (np.array): Coefficients of polynomial that fit left lane
+            right_fit (np.array): Coefficients of polynomial that fit right lane
+            binary (np.array): binary image
+        """
+        self.left_fit = None
+        self.right_fit = None
+        self.binary = None
+        self.nonzero = None
+        self.nonzerox = None
+        self.nonzeroy = None
+        self.clear_visibility = True
+        self.dir = []
+        self.left_curve_img = mpimg.imread('left_turn.png')
+        self.right_curve_img = mpimg.imread('right_turn.png')
+        self.keep_straight_img = mpimg.imread('straight.png')
+        #contrast 
+        self.left_curve_img = cv2.normalize(src=self.left_curve_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        self.right_curve_img = cv2.normalize(src=self.right_curve_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        self.keep_straight_img = cv2.normalize(src=self.keep_straight_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+
+       
+        # HYPERPARAMETERS
+        # Number of sliding windows
+        self.nwindows = 9
+        # Width of the the windows +/- margin
+        self.margin = 100
+        # Mininum number of pixels found to recenter window
+        self.minpix = 50
+
+    def forward(self, img):
+        """Take a image and detect lane lines.
+
+        Parameters:
+            img (np.array): An binary image containing relevant pixels
+
+        Returns:
+            Image (np.array): An RGB image containing lane lines pixels and other details
+        """
+        self.extract_features(img)
+        return self.fit_poly(img)
+
+    def pixels_in_window(self, center, margin, height, img):
+        """ Return all pixel that in a specific window
+
+        Parameters:
+            center (tuple): coordinate of the center of the window
+            margin (int): half width of the window
+            height (int): height of the window
+
+        Returns:
+            pixelx (np.array): x coordinates of pixels that lie inside the window
+            pixely (np.array): y coordinates of pixels that lie inside the window
+        """
+                       #x                  #y
+        topleft = (center[0]-margin, center[1]-height//2)   #point
+        bottomright = (center[0]+margin, center[1]+height//2) #point
+
+        cv2.rectangle(img,topleft, bottomright, color=(0,0,255), thickness=100)
+
+
+        #check if topleft is outside in the left and bottomright is outside in the right
+        condx = (topleft[0] <= self.nonzerox) & (self.nonzerox <= bottomright[0])
+        condy = (topleft[1] <= self.nonzeroy) & (self.nonzeroy <= bottomright[1])
+        return self.nonzerox[condx&condy], self.nonzeroy[condx&condy]
+
+    def extract_features(self, img):
+        """ Extract features from a binary image
+
+        Parameters:
+            img (np.array): A binary image
+        """
+        self.img = img
+        # Height of of windows - based on nwindows and image shape
+        self.window_height = np.int(img.shape[0]//self.nwindows)
+
+        # Identify the x and y positions of all nonzero pixel in the image
+        self.nonzero = img.nonzero()
+        self.nonzerox = np.array(self.nonzero[1]) #Array containing all x coordinates of the lanes
+        self.nonzeroy = np.array(self.nonzero[0]) #Array containing all y coordinates of the lanes
+
+    def find_lane_pixels(self, img):
+        """Find lane pixels from a binary warped image.
+
+        Parameters:
+            img (np.array): A binary warped image
+
+        Returns:
+            leftx (np.array): x coordinates of left lane pixels
+            lefty (np.array): y coordinates of left lane pixels
+            rightx (np.array): x coordinates of right lane pixels
+            righty (np.array): y coordinates of right lane pixels
+            out_img (np.array): A RGB image that use to display result later on.
+        """
+        assert(len(img.shape) == 2)
+
+        # Create an output image to draw on and visualize the result
+        out_img = np.dstack((img, img, img))
+
+        histogram = hist(img)
+        midpoint = histogram.shape[0]//2
+        leftx_base = np.argmax(histogram[:midpoint])
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+        # Current position to be update later for each window in nwindows
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+        y_current = img.shape[0] + self.window_height//2
+
+        # Create empty lists to reveice left and right lane pixel
+        leftx, lefty, rightx, righty = [], [], [], []
+
+        # Step through the windows one by one
+        for _ in range(self.nwindows):
+            y_current -= self.window_height
+            center_left = (leftx_current, y_current)
+            center_right = (rightx_current, y_current)
+
+            good_left_x, good_left_y = self.pixels_in_window(center_left, self.margin, self.window_height , out_img)
+            good_right_x, good_right_y = self.pixels_in_window(center_right, self.margin, self.window_height, out_img)
+
+            # Append these indices to the lists
+            leftx.extend(good_left_x)
+            lefty.extend(good_left_y)
+            rightx.extend(good_right_x)
+            righty.extend(good_right_y)
+
+
+            if len(good_left_x) > self.minpix:
+                leftx_current = np.int32(np.mean(good_left_x))
+            if len(good_right_x) > self.minpix:
+                rightx_current = np.int32(np.mean(good_right_x))
+
+        return leftx, lefty, rightx, righty, out_img
