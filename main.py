1fa10f2bd3ce5a8864d5425dde1514a980e70ba2
Mohamed AbdelAzeem <42822972+MohamedAbdelAzeem@users.noreply.github.com>
2022-04-20 12:35:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import cv2
-#from IPython.display import HTML, Video
+from IPython.core.display import HTML, Video
 from moviepy.editor import VideoFileClip
 
+from LaneLines import *
 
 
 
@@ -18,38 +19,65 @@ drive.mount('/content/drive')
 """
 
 
-#Reading images from files
-images = []
-images.append(mpimg.imread('test_images/challenge_video_frame_1.jpg'))
-images.append(mpimg.imread('test_images/challenge_video_frame_100.jpg'))
-images.append(mpimg.imread('test_images/challenge_video_frame_110.jpg'))
-images.append(mpimg.imread('test_images/challenge_video_frame_120.jpg'))
-images.append(mpimg.imread('test_images/challenge_video_frame_130.jpg'))
-images.append(mpimg.imread('test_images/challenge_video_frame_140.jpg'))
 
-images1 = []
-images1.append(mpimg.imread('test_images/project_video_frame_1032.jpg'))
-images1.append(mpimg.imread('test_images/project_video_frame_1048.jpg'))
-images1.append(mpimg.imread('test_images/project_video_frame_235.jpg'))
-images1.append(mpimg.imread('test_images/project_video_frame_810.jpg'))
-images1.append(mpimg.imread('test_images/project_video_frame_551.jpg'))
+lanelines = LaneLines()
+def process_image(img):
+    # step 1
+    img1 = To_Birdeye(img)
+#     img1 = np.copy(img)
+    
+    # step 2
+    hls = cv2.cvtColor(img1, cv2.COLOR_RGB2HLS)
+    hsv = cv2.cvtColor(img1, cv2.COLOR_RGB2HSV)
+    h_channel = hls[:,:,0]
+    l_channel = hls[:,:,1]
+    s_channel = hls[:,:,2]
+    v_channel = hsv[:,:,2]
+
+    right_lane = threshold_rel(l_channel, 0.8, 1.0)
+    right_lane[:,:750] = 0
+
+    left_lane = threshold_abs(h_channel, 20, 30)
+    left_lane &= threshold_rel(v_channel, 0.7, 1.0)
+    left_lane[:,550:] = 0
+
+    img2 = left_lane | right_lane
+    
+#     img2 = birdeye.forward(img2)
+    
+    # step 3
+    img3 = lanelines.forward(img2)
+    
+
+    # step 4
+    img4 = From_Birdeye(img3)
+
+    out_img = cv2.addWeighted(img, 1, img4, 1, 0)
+    
+    out_img = lanelines.plot(out_img)
+
+    return out_img
+  
+  
+  
+  
+def process_video(self, input_path, output_path):
+      clip = VideoFileClip(input_path)
+      out_clip = clip.fl_image(self.forward)
+      out_clip.write_videofile(output_path, audio=False)  
+  
+  
+def main():
+  args = docopt(__doc__)
+  input = args['INPUT_PATH']
+  output = args['OUTPUT_PATH']
+
+  findLaneLines = FindLaneLines()
+  if args['--video']:
+      findLaneLines.process_video(input, output)
+  else:
+      findLaneLines.process_image(input, output)
 
 
-images2 = []
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_1.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_10.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_100.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_200.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_300.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_400.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_500.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_600.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_700.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_800.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_900.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_1000.jpg'))
-images2.append(mpimg.imread('test_images/harder_challenge_video_frame_1100.jpg'))
-
-
-
-
+if __name__ == "__main__":
+    main()
